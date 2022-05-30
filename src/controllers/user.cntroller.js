826a495211cc9ecd77ff4de29bd4b960c090a91e
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-05-31 00:38:43 +0600

    email exist checking hash password use and user store done

--- src/controllers/user.cntroller.js
+++ src/controllers/user.cntroller.js
@@ -1,18 +1,49 @@
 const User = require('../models/users.model')
+const bcrypt = require("bcryptjs")
 
 /* Register */
 const Register = async (req, res, next) => {
-    
+    try {
+        const { name, email, password } = req.body
+
+        /* exist email checking */
+        const isEmailExist = await User.findOne({ email })
+        if (isEmailExist) {
+            return res.status(409).json({
+                status: false,
+                errors: { message: "E-mail already exist." }
+            })
+        }
+
+        /* Hash password */
+        const hashPassword = await bcrypt.hash(password, 10)
+
+        const newUser = new User({
+            name,
+            email, 
+            password: hashPassword
+        })
+        await newUser.save()
+
+        res.status(200).json({
+            status: true,
+            message: "added Successfully"
+        })
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
 }
 /* User List */
 const Index = async (req, res, next) => {
     try {
+        const results = await User.find()
         res.status(200).json({
-            status:true,
-            message: "List Of Users",
+            status: true,
+            data: results,
         })
     } catch (error) {
-        console.log('errors', error)
+        console.log(error)
         next(error)
     }
 }
