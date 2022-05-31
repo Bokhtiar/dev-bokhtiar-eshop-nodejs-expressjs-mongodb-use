216a93965d987fbdc19fd3a8517a538b613f385f
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-05-31 18:35:25 +0600

    admin can category update and show and delete and update status

--- src/controllers/admin/category.controller.js
+++ src/controllers/admin/category.controller.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const category = require('../../models/category.model')
 
 const Index = async (req, res, next) => {
@@ -32,7 +33,87 @@ const Store = async (req, res, next) => {
     }
 }
 
+const Show = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const result = await category.findById(id)
+        res.status(200).json({
+            status: true,
+            data: result
+        })
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
+}
+
+const Update = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const { name } = req.body
+        await category.findByIdAndUpdate(
+            id,
+            {
+                $set: {
+                    name
+                }
+            }
+        )
+
+        res.status(200).json({
+            status: true,
+            message: "Category Updated Successfully...!"
+        })
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
+}
+
+const Destroy = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        await category.findByIdAndDelete(id)
+        res.status(200).json({
+            status: true,
+            message: "Category Deleted Successfully...!"
+        })
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+}
+
+const Status = async (req, res, next) => {
+    try {
+        const {id} = req.params
+        const value = await category.findById(id)
+        if(value.cat_status == 0){
+            value.cat_status =1
+            await value.save()
+            res.status(200).json({
+                status:true,
+                message: "Category Status Update Successfully...!"
+            })
+        }else{
+            value.cat_status = 0
+            await value.save()
+            res.status(200).json({
+                status:true,
+                message: "Category Status Update Successfully...!"
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
+}
+
 module.exports = {
     Index,
-    Store
+    Store,
+    Show,
+    Update,
+    Destroy,
+    Status
 }
--- src/routes/admin/category.route.js
+++ src/routes/admin/category.route.js
@@ -2,6 +2,10 @@ const adminCategory = require('express').Router()
 const CategoryController = require('../../controllers/admin/category.controller')
 
 adminCategory.get('/', CategoryController.Index);
+adminCategory.get('/status/:id', CategoryController.Status);
 adminCategory.post('/', CategoryController.Store);
+adminCategory.get('/:id', CategoryController.Show);
+adminCategory.put('/:id', CategoryController.Update);
+adminCategory.delete('/:id', CategoryController.Destroy);
 
 module.exports = adminCategory
