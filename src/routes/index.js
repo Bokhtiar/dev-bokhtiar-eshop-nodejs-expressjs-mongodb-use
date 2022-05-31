c5c14d05ab56bb2fb54c33670041408dc63e7871
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-05-31 17:17:05 +0600

    admin permession check route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,11 @@ const appRouter = require('express').Router()
 const userRoute = require('./user/user.route')
 const CategoryRoute = require('./user/category.route')
 const adminRoute = require('./admin/admin.route')
+const permission = require('../middleware/permission.middleware')
 
 appRouter.use('/admin', adminRoute)
+
+
 
 appRouter.use('/user', userRoute)
 appRouter.use('/category', CategoryRoute)
